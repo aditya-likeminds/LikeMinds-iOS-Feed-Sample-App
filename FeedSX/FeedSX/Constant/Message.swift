3c49a2c387500aa7f9a664c39f59529b8ad5508b
pushpendrasingh <pushpendra.singh@likeminds.community>
2023-11-21 12:25:10 +0530

    [LM-10240] Added change post to resource configurable methods

--- FeedSX/FeedSX/Constant/Message.swift
+++ FeedSX/FeedSX/Constant/Message.swift
@@ -9,8 +9,34 @@ import Foundation
 
 public struct MessageConstant {
     private init() {}
-    static let restrictToCreatePost = "You do not have permission to create a post."
+    static let restrictToCreatePost = "You do not have permission to create a %@."
     static let restrictToCommentOnPost = "You do not have permission to comment."
-    static let postingInProgress = "A post is already uploading!"
+    static let postingInProgress = "A %@ is already uploading!"
     static let nofiticationFeedDataNotFound = "Oops! You don't have any notification yet."
+}
+
+enum WordAction: Int {
+    case firstLetterCapitalSingular
+    case allCapitalSingular
+    case allSmallSingular
+    case firstLetterCapitalPlural
+    case allCapitalPlural
+    case allSmallPlural
+}
+
+func pluralizeOrCapitalize(to value: String, withAction action: WordAction) -> String {
+    switch action {
+    case .firstLetterCapitalSingular:
+        return value.capitalized
+    case .allCapitalSingular:
+        return value.uppercased()
+    case .allSmallSingular:
+        return value.lowercased()
+    case .firstLetterCapitalPlural:
+        return (value + "s").capitalized
+    case .allCapitalPlural:
+        return (value + "s").uppercased()
+    case .allSmallPlural:
+        return (value + "s").lowercased()
+    }
 }

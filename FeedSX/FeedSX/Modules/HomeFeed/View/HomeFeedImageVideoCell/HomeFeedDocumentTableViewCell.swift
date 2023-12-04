ac73b0144f32085d2292c3c2f8917562cfdfea27
pushpendrasingh <pushpendra.singh@likeminds.community>
2023-12-04 10:45:12 +0530

    Changed Build number and version - Minor ui fix issue due to merge

--- FeedSX/FeedSX/Modules/HomeFeed/View/HomeFeedImageVideoCell/HomeFeedDocumentTableViewCell.swift
+++ FeedSX/FeedSX/Modules/HomeFeed/View/HomeFeedImageVideoCell/HomeFeedDocumentTableViewCell.swift
@@ -103,6 +103,7 @@ class HomeFeedDocumentTableViewCell: UITableViewCell {
         actionFooterSectionView.setupActionFooterSectionData(feedDataView, delegate: delegate)
         
         topicView.configure(with: feedDataView.topics, isSepratorShown: isSepratorShown)
+        topicView.superview?.isHidden = feedDataView.topics.count < 1
         setupContainerData()
         layoutIfNeeded()
     }
